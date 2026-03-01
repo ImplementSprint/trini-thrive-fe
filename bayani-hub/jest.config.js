0948e7774cdc5ee70fc6bcb076653ae13464ebd7
burritoneeeee <antoneeeeetorres@gmail.com>
2026-03-01 23:47:10 +0800

    Configure Jest for coverage collection (#8) (#9) (#10)
    * Configure Jest for coverage collection
    
    Enable coverage collection and set coverage thresholds.
    
    * Update Jest configuration for coverage collection
    
    Enable coverage collection and set coverage thresholds.
    
    * Add coverage collection and thresholds to Jest config
    
    Enable coverage collection and set coverage thresholds.

--- bayani-hub/jest.config.js
+++ bayani-hub/jest.config.js
@@ -9,6 +9,19 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   modulePathIgnorePatterns: ['<rootDir>/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 85,
+      statements: 85,
+    },
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
