de3989496824983c4c5a98bbf7b2d6ffb8bdc202
Netlopeds <mrproatmcorentiixm@gmail.com>
2026-05-23 02:00:07 +0800

    fixes

--- bayani-hub/jest.config.js
+++ bayani-hub/jest.config.js
@@ -12,9 +12,8 @@ const customJestConfig = {
   modulePathIgnorePatterns: ['<rootDir>/.next/'],
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '!<rootDir>/src/**/*.d.ts',
-    '!<rootDir>/src/app/**/*.{ts,tsx}',
+    '<rootDir>/src/lib/**/*.{ts,tsx}',
+    '!<rootDir>/src/lib/**/*.d.ts',
   ],
   coverageReporters: ['text', 'lcov', 'json-summary'],
   coverageThreshold: {
